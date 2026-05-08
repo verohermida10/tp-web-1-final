c1f5828b3ee6be1304bbd9285994c4102695d68a
verohermida10 <verohermida10@gmail.com>
2026-05-08 16:36:01 -0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,6 +1,113 @@
-const peliculaaccion= {
-    
+const peliculaaccion= [
+    {
+    id: "rascacielos",
+    titulo: "Rascacielos (2018)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BMWY4MDc0ZmUtNzdiMi00YzhiLTliNDMtMTlmMmFlZDU1ZjQwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: La película utilizó una enorme combinación de sets reales y efectos digitales para recrear el gigantesco edificio donde ocurre toda la acción.",
+    trailer: "https://www.youtube.com/embed/R6vuwZl5Pec?si=P6CEoWPrdG71ygl7"
+},
+
+{
+    id: "smith",
+    titulo: "Sr. y Sra. Smith (2005)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BMTUxMzcxNzQzOF5BMl5BanBnXkFtZTcwMzQxNjUyMw@@._V1_.jpg",
+    curiosidad: "💡 Curiosidad: La película mezcla comedia romántica con acción, algo poco común en su momento, lo que la convirtió en un éxito al atraer tanto a fans del romance como del cine de acción.",
+    trailer: "https://www.youtube.com/embed/CZ0B22z22pI?si=Qoo2aQ535tmc6DW1"
+},
+{
+    id: "sanandres",
+    titulo: "Terremoto: La Falla de San Andrés (2015)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BNWJiNDZiZjEtYzM4Zi00MTA1LWI5MTktODlmZWYwODAwMjcwXkEyXkFqcGc@._V1_.jpg",
+    curiosidad: "💡 Curiosidad: Para lograr los efectos del terremoto, se combinaron escenarios reales con enormes sets que se destruían físicamente, junto con CGI, creando escenas de desastres mucho más impactantes.",
+    trailer: "https://www.youtube.com/embed/c2vupbrgySk?si=9u72apdx0vW3sVE8"
+},
+
+{
+    id: "deathrace1",
+    titulo: "Death Race (2008)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BYTkzYWY5ZjYtOGI3My00OWE0LTkwOTMtZjVkMWJjZjhjODg0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: Muchos de los autos usados en la película fueron construidos realmente con armaduras y armas funcionales para que las escenas de choque se vieran más reales.",
+    trailer: "https://www.youtube.com/embed/GsaTb2O31rQ?si=RHNgVvLPGJZvKxvV"
+},
+{
+    id: "deathrace2",
+    titulo: "Death Race 2 (2010)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BZTliOWJiY2YtYmIyMC00MWQzLTgyMzMtYjMwZGY5MGIxMGI1XkEyXkFqcGc@._V1_.jpg",
+    curiosidad: "💡 Curiosidad: Aunque salió después de la película de 2008, en realidad funciona como precuela y muestra el origen del famoso corredor Frankenstein.",
+    trailer: "https://www.youtube.com/embed/e9L88V0yh1U?si=jNhOS51zBvIg0M64"
+},
+
+{
+    id: "ff1",
+    titulo: "Rápidos y Furiosos (2001)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BZDU0Yjk5MTctZDI3OS00YmM4LWIxOTMtYTU2NjYyYzdkMWJlXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: La película se inspiró en un artículo real sobre carreras callejeras ilegales publicado en una revista, lo que ayudó a construir un mundo más auténtico alrededor de este fenómeno.",
+    trailer: "https://www.youtube.com/embed/-oJHZre7XZY?si=0VSLbqUWVVZRLW-d"
+},
+
+{
+    id: "ff2",
+    titulo: "2 Fast 2 Furious (2003)",
+    imagen: "https://images.justwatch.com/poster/300636230/s718/2-fast-2-furious.jpg",
+    curiosidad: "💡 Curiosidad: Fue la única película principal de la saga sin Vin Diesel hasta ese momento, centrando toda la historia en Brian O'Conner.",
+    trailer: "https://www.youtube.com/embed/8gf7FEmBDDA?si=Vv-Pie8xog4cFu1t"
+},
+
+{
+    id: "ff4",
+    titulo: "Rápidos y Furiosos 4 (2009)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BZDQzMTEzYzktNjhjNC00N2IwLWIzN2ItMGQ1MjBkNGNhY2VmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: Esta entrega marcó el regreso del elenco original, retomando la historia principal de la saga y dejando de lado el enfoque más independiente que habían tenido las películas anteriores.",
+    trailer: "https://www.youtube.com/embed/_ixlOH9EzL4?si=Sag8fgyc9Lmdnyv5"
+},
+{
+    id: "ff5",
+    titulo: "Rápidos y Furiosos 5 (2011)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BMTUxNTk5MTE0OF5BMl5BanBnXkFtZTcwMjA2NzY3NA@@._V1_.jpg",
+    curiosidad: "💡 Curiosidad: La famosa escena del robo de la caja fuerte fue filmada con una caja real arrastrada por autos en algunas tomas, combinada con efectos visuales para aumentar el impacto.",
+    trailer: "https://www.youtube.com/embed/ndth_OIyfJw?si=RFbaEU31zqYUPK2B"
+},
+
+{
+    id: "ff6",
+    titulo: "Rápidos y Furiosos 6 (2013)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: La pista de aterrizaje del final se volvió un meme porque parece interminable durante toda la persecución del avión.",
+    trailer: "https://www.youtube.com/embed/yY6JUPCgDxU?si=Sr-x0zRIMw2x4QtI"
+},
+
+{
+    id: "ff7",
+    titulo: "Rápidos y Furiosos 7 (2015)",
+    imagen: "https://es.web.img3.acsta.net/pictures/15/02/09/15/57/551764.jpg",
+    curiosidad: "💡 Curiosidad: Tras la muerte de Paul Walker, sus hermanos ayudaron a completar sus escenas junto con efectos digitales, permitiendo terminar la película y darle un cierre emotivo a su personaje.",
+    trailer: "https://www.youtube.com/embed/tX0MGnC41nk?si=IyC3byGaDKQikrQY"
+},
+
+{
+    id: "ff8",
+    titulo: "Rápidos y Furiosos 8 (2017)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BNDVlNmZkMjAtNGUwNy00MGYzLWJiNjYtZDdhZDRlMjNkMGZhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    curiosidad: "💡 Curiosidad: Varias escenas de persecución fueron filmadas en Islandia usando autos reales sobre hielo y nieve extrema.",
+    trailer: "https://www.youtube.com/embed/K9ApecASdpA?si=fOhtRk7whOqPgKjQ"
+},
+{
+    id: "ff9",
+    titulo: "Rápidos y Furiosos 9 (2021)",
+    imagen: "https://m.media-amazon.com/images/M/MV5BNWEwNWExNWUtODI1OC00NzYzLTk4ZTctYmY4ZTBjMWQzMjMwXkEyXkFqcGc@._V1_.jpg",
+    curiosidad: "💡 Curiosidad: Esta fue la película donde la saga finalmente llegó al espacio, algo que durante años había sido un meme entre los fans.",
+    trailer: "https://www.youtube.com/embed/3YNwcdVkpZQ?si=cdEEnv_t1zXslV5F"
+},
+
+{
+    id: "ff10",
+    titulo: "Rápidos y Furiosos 10 (2023)",
+    imagen: "https://www.universalpictures.com.ar/tl_files/content/movies/fastx/poster/01.jpg",
+    curiosidad: "💡 Curiosidad: La décima entrega lleva las escenas de acción a un nivel extremo, combinando efectos prácticos con CGI para crear secuencias cada vez más exageradas y espectaculares.",
+    trailer: "https://www.youtube.com/embed/KOQy0AwA7k8?si=WJ_Tmw3C08zWejPo"
 }
+]
+    
 
 const peliculasDeporte = [
     {
@@ -94,3 +201,47 @@ function mostrarPeliculasDeporte() {
 }
 
 mostrarPeliculasDeporte();
+function mostrarPeliculasAccion() {
+
+    const contenedor = document.getElementById("contenedor-accion");
+
+    peliculaaccion.forEach(peli => {
+
+        contenedor.innerHTML += `
+        
+        <div class="pelicula">
+
+            <img src="${peli.imagen}">
+
+            <h3>${peli.titulo}</h3>
+
+            <button onclick="like('${peli.id}')">
+                ❤️ Me gusta
+            </button>
+
+            <p id="${peli.id}">Likes: 0</p>
+
+            <div class="estrellas">
+                <span onclick="rate(this,1)">⭐</span>
+                <span onclick="rate(this,2)">⭐</span>
+                <span onclick="rate(this,3)">⭐</span>
+                <span onclick="rate(this,4)">⭐</span>
+                <span onclick="rate(this,5)">⭐</span>
+            </div>
+
+            <p class="curiosidad">
+                ${peli.curiosidad}
+            </p>
+
+            <iframe 
+                class="pelicula-avance"
+                src="${peli.trailer}"
+                allowfullscreen>
+            </iframe>
+
+        </div>
+        `;
+    });
+}
+
+mostrarPeliculasAccion();
